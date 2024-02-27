b99ee635f153ec88d34b804353d696d993c45294
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-02-27 15:40:51 +0000

    added feat. to add and remove room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,35 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @room = Room.new
     @rooms = Room.public_room
+    @single_room = Room.find(params[:id])
     @users = User.all_except(current_user)
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.turbo_stream { render turbo_stream: turbo_stream.append(:rooms, partial: 'rooms/room', locals: { room: @room }) }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('flash-messages', partial: 'layouts/flash_messages', locals: { flash: flash }) }
+      end
+    end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@room) }
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name) # Adjust as per your Room model attributes
   end
 end
